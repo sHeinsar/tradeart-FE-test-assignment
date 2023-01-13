10b1acb650cce3665d2435a9a8de1f7526fa7368
Sander Heinsar <sander.heinsar@mountbirch.com>
2023-01-13 16:51:44 +0200

    Fixed test folder structure

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from '../components/App'
 
-test('renders sup', () => {
+test('renders text', () => {
   render(<App />)
   const linkElement = screen.getByText(/sup/i)
   expect(linkElement).toBeInTheDocument()
