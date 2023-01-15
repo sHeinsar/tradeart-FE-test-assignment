a88ddf0d88f5fd3039ea12c48412774a043c0da7
Sander Heinsar <sander.heinsar@mountbirch.com>
2023-01-15 18:30:29 +0200

    removed unused tests

--- src/tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from '../components/pages/App'
-
-test('renders text', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/sup/i)
-  expect(linkElement).toBeInTheDocument()
-})
